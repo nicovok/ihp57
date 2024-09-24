33f2ce68297cf600c550dcaf2153810c104088df
Prouteau <prouteaunico@gmail.com>
2024-09-24 19:27:52 +0200

    youtube channel added to linktree

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,38 +1,43 @@
 type Link = {
-    label: string;
-    url: string;
-    icon: string;
+  label: string;
+  url: string;
+  icon: string;
 };
 
 export const links: Link[] = [
-    {
-        label: 'Web',
-        url: '/',
-        icon: '/icons/globe.svg',
-    },
-    {
-        label: 'Facebook',
-        url: 'https://www.facebook.com/profile.php?id=100082885465174',
-        icon: '/icons/facebook.svg',
-    },
-    {
-        label: 'Instagram',
-        url: 'https://www.instagram.com/icehockeypro57/',
-        icon: '/icons/instagram.svg',
-    },
-    {
-        label: 'TikTok',
-        url: 'https://www.tiktok.com/@icehockeypro57',
-        icon: '/icons/hexagon.svg',
-    },
-    {
-        label: 'Youtube',
-        url: 'https://www.youtube.com/watch?v=D49v9QJriWA&list=PLpxKi3C8s3ARFx1yRashh2tBKYqLf9IGq',
-        icon: '/icons/youtube.svg',
-    },
-    {
-        label: 'LinkedIn',
-        url: 'https://hu.linkedin.com/in/attila-orb%C3%A1n-542600258',
-        icon: '/icons/linkedin.svg',
-    },
+  {
+    label: "Web",
+    url: "/",
+    icon: "/icons/globe.svg",
+  },
+  {
+    label: "Facebook",
+    url: "https://www.facebook.com/profile.php?id=100082885465174",
+    icon: "/icons/facebook.svg",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/icehockeypro57/",
+    icon: "/icons/instagram.svg",
+  },
+  {
+    label: "TikTok",
+    url: "https://www.tiktok.com/@icehockeypro57",
+    icon: "/icons/hexagon.svg",
+  },
+  {
+    label: "Youtube",
+    url: "https://www.youtube.com/@ihp57",
+    icon: "/icons/youtube.svg",
+  },
+  {
+    label: "Playlist",
+    url: "https://www.youtube.com/watch?v=D49v9QJriWA&list=PLpxKi3C8s3ARFx1yRashh2tBKYqLf9IGq",
+    icon: "/icons/youtube.svg",
+  },
+  {
+    label: "LinkedIn",
+    url: "https://hu.linkedin.com/in/attila-orb%C3%A1n-542600258",
+    icon: "/icons/linkedin.svg",
+  },
 ];
