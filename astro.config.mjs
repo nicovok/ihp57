9f975bc1dd827373392a82d2110cd13518d6938b
Prouteau <nicolas.prouteau.2007@gmail.com>
2024-09-25 22:37:32 +0200

    change astro output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel/serverless';
 
 import react from '@astrojs/react';
@@ -10,10 +9,9 @@ import { defaultLang } from './src/i18n/ui';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    output: 'hybrid',
+    output: 'server',
     adapter: vercel({
         imageService: true,
-        devImageService: true,
         webAnalytics: {
             enabled: true,
         },
