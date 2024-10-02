a9e3f012cfc5fb3a1d523c02ac2a6a782ddbb333
Prouteau <nicolas.prouteau.2007@gmail.com>
2024-10-02 22:56:30 +0200

    getting rid of image tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
-import { astroImageTools } from "astro-imagetools";
 
 import { defaultLang } from "./src/i18n/ui";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroImageTools],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel({
     imageService: true,
