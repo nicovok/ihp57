1bf6bab087b7f8899293228f9edd9d1e9ced3071
Prouteau <nicolas.prouteau.2007@gmail.com>
2024-10-02 22:27:39 +0200

    add astro imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import { astroImageTools } from "astro-imagetools";
 
-import { defaultLang } from './src/i18n/ui';
+import { defaultLang } from "./src/i18n/ui";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
-    output: 'server',
-    adapter: vercel({
-        imageService: true,
-        webAnalytics: {
-            enabled: true,
-        },
-        speedInsights: {
-            enabled: true,
-        },
-    }),
-    redirects: {
-        '/': '/' + defaultLang + '/',
+  integrations: [tailwind(), react(), astroImageTools],
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
     },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
+  redirects: {
+    "/": "/" + defaultLang + "/",
+  },
 });
