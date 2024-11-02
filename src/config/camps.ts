cc7b13d7797fdc0954aabdea2206426248a5f097
Prouteau Nicolas <nicolas.prouteau.2007@gmail.com>
2024-11-02 17:29:19 +0100

    add 2025 summer camps

--- src/config/camps.ts
+++ src/config/camps.ts
@@ -21,6 +21,18 @@ export const camps: Camp[] = [
   },
   {
     name: {
+      hu: "Nyári Táborok 2025",
+      en: "Summer Camps 2025",
+    },
+    description: {
+      hu: "Egész nyáron IHP57 tábor, 6 turnusban. Jelentkezz az alábbi gombra kattintva!",
+      en: "IHP57 camp throughout the whole summer, in 6 rounds. Register by clicking the button below!",
+    },
+    image: "/images/camps/summer.jpg",
+    link: "https://forms.gle/HC5JqxzGjDpRs7RT9",
+  },
+  {
+    name: {
       hu: "Kapus Táborok",
       en: "Goalie Camps",
     },
