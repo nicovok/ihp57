57e92449f32451fb84288dad0834918c86af7fda
Prouteau Nicolas <nicolas.prouteau.2007@gmail.com>
2024-11-06 23:27:25 +0100

    add mini camp

--- src/config/camps.ts
+++ src/config/camps.ts
@@ -17,7 +17,7 @@ export const camps: Camp[] = [
       hu: "Egész évben futó képzéseink.",
       en: "Two-day camps every weekend.",
     },
-    link: "https://docs.google.com/forms/d/e/1FAIpQLSc8d67_lrYK-CvRNr9yQNG3N61QBTRh_BzNZRV8bhJyJl78Lw/viewform",
+    link: "https://forms.gle/hUntotvwnrwNwXJz7",
   },
   {
     name: {
